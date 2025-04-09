d8161df2589fc556eae651e5fce73dc6dad3e865
Karthik <karthik@Karthiks-MacBook-Pro.local>
2025-04-09 22:32:52 +0530

    started with simple recursive and iterative approaches

---
+++ bigonacci.py
@@ -0,0 +1,37 @@
+from timeit import timeit
+
+bigonacci_result = {}
+
+def recursive_simple(n):
+    if n == 0 or n == 1:
+        bigonacci_result["recursive_simple"] = {"n" : n, "f(n)" : n}
+        return bigonacci_result["recursive_simple"]["f(n)"]
+    else:
+        bigonacci_result["recursive_simple"] = {"n" : n, "f(n)" : recursive_simple(n - 1) + recursive_simple(n - 2)}
+        return bigonacci_result["recursive_simple"]["f(n)"]
+    
+def iterative_simple(n):
+    if n == 0 or n == 1:
+        return n
+    else:
+        n_0, n_1 = 0, 1
+        for n_2 in range(2, n + 1):
+            n_2 = n_1 + n_0
+            n_0, n_1 = n_1, n_2
+        bigonacci_result["iterative_simple"] = {"n" : n, "f(n)" : n_2}
+        return bigonacci_result["iterative_simple"]["f(n)"]
+
+approaches = [recursive_simple]
+max_n = 10000000
+
+for fn in approaches:
+    for n in range(0, max_n):
+        t = timeit(f"fn({n})", number=1, globals=globals())
+        print(n, t)
+        if t >= 1:
+            break
+
+import json
+
+with open("output.json", "w") as output_file:
+    json.dump(bigonacci_result, output_file) 
