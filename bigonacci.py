bdbade77efc77bd120c0a0a89c57bab27093dacc
Karthik <karthik@Karthiks-MacBook-Pro.local>
2025-04-09 22:54:26 +0530

    memoised hit recursion max at 996

--- bigonacci.py
+++ bigonacci.py
@@ -1,5 +1,8 @@
 from timeit import timeit
 
+import sys
+sys.set_int_max_str_digits(1000000) 
+
 bigonacci_result = {}
 
 def recursive_simple(n):
@@ -21,12 +24,23 @@ def iterative_simple(n):
         bigonacci_result["iterative_simple"] = {"n" : n, "f(n)" : n_2}
         return bigonacci_result["iterative_simple"]["f(n)"]
 
-approaches = [recursive_simple]
-max_n = 10000000
+memo = {0 : 0, 1 : 1}
+def recursive_memoised(n):
+    if n in memo:
+        bigonacci_result["recursive_memoised"] = {"n" : n, "f(n)" : memo[n]}
+        return bigonacci_result["recursive_memoised"]["f(n)"]
+    else:
+        bigonacci_result["recursive_memoised"] = {"n" : n, "f(n)" : recursive_memoised(n - 1) + recursive_memoised(n - 2)}
+        memo[n] = bigonacci_result["recursive_memoised"]["f(n)"]
+        return bigonacci_result["recursive_memoised"]["f(n)"]
+    
+approaches = [recursive_memoised]
+max_n = 996
 
 for fn in approaches:
-    for n in range(0, max_n):
+    for n in range(0, max_n, 1):
         t = timeit(f"fn({n})", number=1, globals=globals())
+        memo = {0 : 0, 1 : 1}
         print(n, t)
         if t >= 1:
             break
